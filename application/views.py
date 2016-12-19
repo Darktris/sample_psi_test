02649667aed17b70ef86c3f9308e9481e4e2e54d
Daniel Perdices <daniel.perdices@estudiante.uam.es>
2016-12-19 21:28:59 +0100

    We got a 8

--- application/views.py
+++ application/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from application.models import *
 # Create your views here.
+def prescription(request):
+	a = Prescription.objects.all()
+	context_dict = { 'prescriptions': Prescription.objects.all()}
+	return render(request, 'application/prescription.html', context_dict)
--- project/urls.py
+++ project/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^application/', include('application.urls')),
+] + staticfiles_urlpatterns()
